250514ce932e4f37c8f73ad25e9d92bba9fcc6ef
healthypackrat <healthypackrat@gmail.com>
2019-11-03 23:07:16 +0900

    Updates

---
+++ precache-manifest.2733076e93ae3aaa604db5ce4d882209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4a73fe4d6be50626fd1e42fc081a80",
+    "url": "/rubygems-class-table/index.html"
+  },
+  {
+    "revision": "76ada5f76688c9a534de",
+    "url": "/rubygems-class-table/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "18009272cdd5a9744ce0",
+    "url": "/rubygems-class-table/static/css/main.42d05314.chunk.css"
+  },
+  {
+    "revision": "76ada5f76688c9a534de",
+    "url": "/rubygems-class-table/static/js/2.f6f4cab5.chunk.js"
+  },
+  {
+    "revision": "18009272cdd5a9744ce0",
+    "url": "/rubygems-class-table/static/js/main.ce860a26.chunk.js"
+  },
+  {
+    "revision": "f453f29554a64e0b0848",
+    "url": "/rubygems-class-table/static/js/runtime-main.6f655d34.js"
+  }
+]);
