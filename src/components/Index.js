9918144a45fb67bca615de37adb7b732d161473e
healthypackrat <healthypackrat@gmail.com>
2019-11-06 08:13:30 +0900

    Display loading message on Index

--- src/components/Index.js
+++ src/components/Index.js
@@ -9,24 +9,37 @@ class Index extends React.Component {
   }
 
   render() {
+    let content;
+
+    if (this.props.isFetching) {
+      content = <p className="text-center">Loading...</p>;
+    } else {
+      content = (
+        <ul>
+          {this.props.items.map(item => {
+            return <li key={item}><Link to={`/gems/${item}`}>{item}</Link></li>;
+          })}
+        </ul>
+      );
+    }
+
     return (
       <div className="main container-fluid my-3">
         <ol className="breadcrumb">
           <li className="breadcrumb-item active">Home</li>
         </ol>
         <p><a href="https://github.com/healthypackrat/rubygems-class-table">リポジトリに戻る</a></p>
-        <ul>
-          {this.props.items.map(item => {
-            return <li key={item}><Link to={`/gems/${item}`}>{item}</Link></li>;
-          })}
-        </ul>
+        {content}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return { items: state.manifest.items };
+  return {
+    isFetching: state.manifest.isFetching,
+    items: state.manifest.items
+  };
 };
 
 export default connect(mapStateToProps)(Index);
